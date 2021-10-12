b08ea6062ec50eeb61ab22e76b01cc7be8a3a8fc
Jamesbondsamit <78743840+Jamesbondsamit@users.noreply.github.com>
2021-10-12 14:48:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+function preload(){
+clone_nose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png')
+}
+function setup(){
+    canvas=createCanvas(380, 400);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,400);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+   
+}
+ function modelLoaded(){
+     console.log('Posenet is loaded ');
+
+ }
+
+ function gotPoses(results){
+     if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+         console.log("nosex="+noseX);
+         console.log("nose y="+noseY);
+     }
+ }
+
+ function draw(){
+    image(video,0,0,380,400);
+image(clone_nose,noseX-15,noseY-15,30,30);
+ }
+function take_snapshot(){
+    save('st676767676.png');
+
+}
+
